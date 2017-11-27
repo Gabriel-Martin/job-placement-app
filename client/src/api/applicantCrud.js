9b5be7a4471577e61ff0e69d899c7ac6fa7355e7
Calvin Rafferty <calvinhobbes352@gmail.com>
2017-11-27 14:46:16 -0800

    api crud basic done

--- client/src/api/applicantCrud.js
+++ client/src/api/applicantCrud.js
@@ -1,1 +1,66 @@
-"placeholder";
+const baseURL = (path = "") => `http://loaclhost:4040/api/applicant/${path}`;
+
+const signUp = data => {
+  return fetch(baseURL(), {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .catch(error => console.log(error));
+};
+
+const login = credentials => {
+  return fetch(baseURL("login"), {
+    method: "POST",
+    body: JSON.stringify(credentials),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .catch(error => console.log(error));
+};
+
+const update = (id, data) => {
+  return fetch(baseURL(id), {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .catch(error => console.log(error));
+};
+
+const remove = id => {
+  return fetch(baseURL(id), {
+    method: "DELETE"
+  })
+    .then(response => response.json())
+    .catch(error => console.log(error));
+};
+
+const getAll = () => {
+  return fetch(baseURL())
+    .then(response => response.json())
+    .catch(error => console.log(error));
+};
+
+const getById = id => {
+  return fetch(baseURL(id))
+    .then(response => response.json())
+    .catch(error => console.log(error));
+};
+
+export default {
+  signUp,
+  login,
+  update,
+  remove,
+  getAll,
+  getById
+};
