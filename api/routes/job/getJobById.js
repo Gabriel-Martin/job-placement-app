87b3b9b5f80010c418bd6166ecb644b045da7d95
Caleb Madson <madsoncaleb@gmail.com>
2017-11-28 08:53:53 -0800

    removed auth from getJobById api side

--- api/routes/job/getJobById.js
+++ api/routes/job/getJobById.js
@@ -2,10 +2,12 @@ module.exports = {
   path: "/api/job/{jobId}",
   method: "GET",
   config: {
+    auth: { mode: "optional" },
     handler: function(request, reply) {
       let jobId = request.params.jobId;
 
-      this.models.Job.get(jobId)
+      this.models.Job
+        .get(jobId)
         .getJoin({ applications: true, company: true })
         .then(result => reply(result))
         .catch(err => {
