cd63d62387035ad541706897490f4b1eb8cbacd1
Calvin Rafferty <calvinhobbes352@gmail.com>
2017-11-28 12:00:54 -0800

    this sucks

---
+++ client/src/views/applicant/ApplicantProfile.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+// import api from "../../api/applicantCrud";
+
+import applicant from "../../api/applicantCrud";
+
+class ApplicantProfile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      applications: [],
+      jobs: []
+    };
+  }
+
+  componentDidMount() {
+    applicant.getCurrent().then(applicant => {
+      this.setState(state => {
+        return {
+          ...applicant
+        };
+      });
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h1>Applicant Profile</h1>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid black",
+              width: "20%",
+              height: "100vh"
+            }}
+          >
+            <h3>Interested</h3>
+            <div>
+              {this.state.jobs.map(j => <div key={j.id}>{j.position}</div>)}
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid black",
+              width: "20%",
+              height: "100vh"
+            }}
+          >
+            <h3>Applied</h3>
+            <div>
+              {this.state.applications.map(app => (
+                <div key={app.id}>{app.job.position}</div>
+              ))}
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid black",
+              width: "20%",
+              height: "100vh"
+            }}
+          >
+            <h3>Processing</h3>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid black",
+              width: "20%",
+              height: "100vh"
+            }}
+          >
+            <h3>Status</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ApplicantProfile;
