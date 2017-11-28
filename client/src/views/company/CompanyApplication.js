f9f867a94ea8c38f8714bcefe8a6267dff442bde
Sarah Nicolls <sarah.goossen@hotmail.com>
2017-11-28 15:28:41 -0800

    added styled-components

--- client/src/views/company/CompanyApplication.js
+++ client/src/views/company/CompanyApplication.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import application from "../../api/applicationCrud";
 
 class CompanyApplication extends Component {
@@ -25,15 +26,32 @@ class CompanyApplication extends Component {
     console.log(this.state.applications);
     let { applications } = this.state;
     return (
-      <div>
-        <h1>Application</h1>
+      <Containter>
+        <Title>Application</Title>
         <div>
+          Name:
           {applications.firstName} {applications.lastName}
         </div>
-      </div>
+        <div>Address: {applications.address}</div>
+        <div>City: {applications.city}</div>
+        <div>State: {applications.state}</div>
+        <div>Zip: {applications.zip}</div>
+        <div>Email: {applications.email}</div>
+        <div>Phone: {applications.phone}</div>
+        <div>Education: {applications.education}</div>
+      </Containter>
     );
   }
 }
 
+const Containter = styled.div`
+  background-color: #d3d3d3;
+  min-height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 100px;
+  text-align: center;
+`;
+
 export default CompanyApplication;
-// this should just be displaying one application not all of them
